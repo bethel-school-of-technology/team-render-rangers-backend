ca1aae62336d581024edba311a0f30cd519ecc1d
Ketsy <ketsy.prophete@gmail.com>
2024-11-11 18:37:52 -0500

    created recipe model

---
+++ Models/Recipe.cs
@@ -0,0 +1,8 @@
+public class Recipe {
+    public int RecipeId { get; set; }
+    public required string RecipeName { get; set; }
+    public required string RecipeCategory { get; set; }
+    public required string RecipeIngredients { get; set; }
+    public required string RecipeInstructions { get; set; }
+    public required string RecipeImgUrl { get; set; }
+}
