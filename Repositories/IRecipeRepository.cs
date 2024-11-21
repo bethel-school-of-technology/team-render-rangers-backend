732096ad4e73818a46bc50dc45363aba1be72c31
Ketsy <ketsy.prophete@gmail.com>
2024-11-21 14:46:04 -0500

    added getRecipe()

--- Repositories/IRecipeRepository.cs
+++ Repositories/IRecipeRepository.cs
@@ -5,6 +5,8 @@ namespace feastly_api.Repositories;
 public interface IRecipeRepository {
     Task<IEnumerable<Recipe>> GetAllRecipes();
 
+    Task<Recipe> GetRecipe(int recipeId);
+
     Task<Recipe> CreateRecipe(Recipe newRecipe);
 
     Task DeleteRecipe(int recipeId);
