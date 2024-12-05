3a92d234ab70c2bfbef4b68a5bdcba088951524f
Ketsy <ketsy.prophete@gmail.com>
2024-12-04 21:07:01 -0500

    created authService

---
+++ Repositories/AuthService.cs
@@ -0,0 +1,73 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using feastly_api.Migrations;
+using feastly_api.Models;
+using Microsoft.IdentityModel.Tokens;
+using bcrypt = BCrypt.Net.BCrypt;
+
+namespace feastly_api.Repositories;
+
+public class AuthService : IAuthService
+{
+    private static RecipeDbContext _context;
+    private static IConfiguration _config;
+
+    public AuthService(RecipeDbContext context, IConfiguration config)
+    {
+        _context = context;
+        _config = config;
+    }
+
+    public User CreateUser(User user)
+    {
+        var passwordHash = bcrypt.HashPassword(user.Password);
+        user.Password = passwordHash;
+
+        _context.Add(user);
+        _context.SaveChanges();
+        return user;
+    }
+
+    public string SignIn(string email, string password)
+    {
+        var user = _context.Users.SingleOrDefault(x => x.Email == email);
+        var verified = false;
+
+        if (user != null)
+        {
+            verified = bcrypt.Verify(password, user.Password);
+        }
+
+        if (user == null || !verified)
+        {
+            return String.Empty;
+        }
+
+        return BuildToken(user);
+    }
+
+    private string BuildToken(User user)
+    {
+        var secret = _config.GetValue<String>("TokenSecret");
+        var signignKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+
+        var signingCredentials = new SigningCredentials(signignKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new Claim[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserId.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email ?? ""),
+            new Claim(JwtRegisteredClaimNames.Name, user.Name ?? "")
+        };
+
+        var jwt = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(5),
+            signingCredentials: signingCredentials);
+
+        var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+        return encodedJwt;
+    }
+}
