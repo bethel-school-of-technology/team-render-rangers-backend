01c344f186063bf49b01cdae041e28a469cfe6e8
Ketsy <ketsy.prophete@gmail.com>
2024-12-09 18:53:08 -0500

    added seeded recipe entity to EF

--- Migrations/20241209230343_SeededRecipeEntity.Designer.cs
+++ Migrations/20241209230343_SeededRecipeEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using feastly_api.Migrations;
 
@@ -11,9 +12,11 @@ using feastly_api.Migrations;
 namespace feastly_api.Migrations
 {
     [DbContext(typeof(RecipeDbContext))]
-    partial class RecipeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241209230343_SeededRecipeEntity")]
+    partial class SeededRecipeEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
